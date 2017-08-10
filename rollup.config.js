ee71a3d42be7285a6d7b8b7462c0b0c84cfe6bda
Adrian Stainforth <adrian.stainforth@gmail.com>
2017-08-10 11:19:06 +0100

    fixing dependencies

---
+++ rollup.config.js
@@ -0,0 +1,34 @@
+// rollup.config.js
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+  entry: 'src/geo.js'
+  , format: 'cjs'
+  , moduleName: 'BetterMaps'
+  , sourceMap: true
+  , plugins: [
+    resolve({
+      browser: true
+      , extensions: [ '.js' ]
+    })
+    , commonjs()
+    , babel({
+      exclude: 'node_modules/**' // only transpile our source code
+      , externalHelpers: true
+      , plugins: ['external-helpers']
+      // , runtimeHelpers: true
+    })
+    , replace({
+      ENVIRONMENT: JSON.stringify(process.env.NODE_ENV)
+    })
+    , uglify()
+  ]
+  ,  external: [
+    'lodash'
+  ]
+  , dest: 'index.js'
+};
